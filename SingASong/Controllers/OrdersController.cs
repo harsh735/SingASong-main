6c46a3dbcd5831f99894a593199b30d5d076f2e2
harshendra.singh <harshendra.singh@G1-HPML114-L>
2024-01-28 20:06:59 +0530

    Add project files.

---
+++ SingASong/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SingASong.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Payments()
+        {
+            return View();
+        }
+
+        public IActionResult PaymentsThanks()
+        {
+            return View();  
+        }
+    }
+}
---
+++ SingASong/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SingASong.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductCatalogue()
+        {
+            return View();
+        }
+    }
+}
